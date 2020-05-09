e18a5ddf749ea12db6577a9b6e5a8a4f529bccbd
MadhavBahlMD <madhavbahl10@gmail.com>
2020-05-09 22:27:12 +0530

    Updates

---
+++ precache-manifest.79ab1b816a76e9766dceeb7bc7c08ef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fad3284a08956a6b93e91336b7d02054",
+    "url": "/name-startup/index.html"
+  },
+  {
+    "revision": "55d915b85cd952d5505d",
+    "url": "/name-startup/static/css/main.11a33819.chunk.css"
+  },
+  {
+    "revision": "a4646a883fb6043014cb",
+    "url": "/name-startup/static/js/2.87788b9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/name-startup/static/js/2.87788b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55d915b85cd952d5505d",
+    "url": "/name-startup/static/js/main.e42b536a.chunk.js"
+  },
+  {
+    "revision": "55429c0f558be159a062",
+    "url": "/name-startup/static/js/runtime-main.2412d62b.js"
+  }
+]);
